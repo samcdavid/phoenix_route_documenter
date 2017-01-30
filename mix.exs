3338f9a9cc48f4d9e5032dbff579b291b94cd738
Sam McDavid <mcdavidsa@gmail.com>
2017-01-30 14:22:05 -0500

    Prep Release
    Why
    ---
    
    - Documentation needs to be up to date before publishing.
    
    This change addresses the need by
    ---------------------------------
    
    - Update mix and add license.
    
    Next Steps
    ----------
    
    - Deploy

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule PhoenixRouteDocumenter.Mixfile do
     [
       app: :phoenix_route_documenter,
       build_embedded: Mix.env == :prod,
+      description: description(),
       deps: deps(),
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
+      package: package(),
       preferred_cli_env: [coveralls: :test],
       start_permanent: Mix.env == :prod,
       test_coverage: [tool: ExCoveralls],
@@ -26,11 +28,30 @@ defmodule PhoenixRouteDocumenter.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp description do
+    """
+    A package for reading a Phoenix Route Module and generating a map from the
+    output that can be serialized to JSON.
+    """
+  end
+
   defp deps do
     [
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:excoveralls, "~> 0.5", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:phoenix, "~> 1.2.1"},
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "README*", "LICENSE*", "mix.exs"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/samcdavid/phoenix_route_documenter",
+      },
+      maintainers: ["Sam McDavid"],
     ]
   end
 end
